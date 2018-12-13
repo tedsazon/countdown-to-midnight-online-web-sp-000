f8153e3de97debe770c05ad400426a41f7797692
Ted Sazon <tedsazon@gmail.com>
2018-12-13 18:47:18 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,12 @@
-#write your code here
+require 'pry'
 
 def countdown
+  number = 10
+  while number > 0
+    puts "#{number} SECOND(S)!"
+  binding.pry
+    number -= 1
+
+  end
+  puts "HAPPY NEW YEAR!"
 end
